57b708ef2e7d2976c5c736f2e28ebc16fbd232f7
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-02-28 17:53:08 -0500

    Add math file with some examples of tests

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,14 @@
+const { sum, multiply, divide } = require("./02-math");
+
+test("adds 1 + 3 to equal 4", () => {
+  expect(sum(1, 3)).toBe(4);
+});
+
+test("multiplies 2 * 3 to equal 6", () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+test("should have the right division result", () => {
+  expect(divide(6, 3)).toBe(2);
+  expect(divide(10, 0)).toBeNull();
+});
