1d7a5b30b360c5cf00956ecde91263ba84e32490
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-03-02 17:52:56 -0500

    Update math test file with describe function

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,14 +1,20 @@
 const { sum, multiply, divide } = require('./02-math');
 
-test('adds 1 + 3 to equal 4', () => {
-  expect(sum(1, 3)).toBe(4);
-});
-
-test('multiplies 2 * 3 to equal 6', () => {
-  expect(multiply(2, 3)).toBe(6);
-});
-
-test('should have the right division result', () => {
-  expect(divide(6, 3)).toBe(2);
-  expect(divide(10, 0)).toBeNull();
+describe('Math tests', () => {
+  describe('sum tests', () => {
+    test('adds 1 + 3 to equal 4', () => {
+      expect(sum(1, 3)).toBe(4);
+    });
+  });
+  describe('multiply tests', () => {
+    test('multiplies 2 * 3 to equal 6', () => {
+      expect(multiply(2, 3)).toBe(6);
+    });
+  });
+  describe('divide tests', () => {
+    test('should have the right division result', () => {
+      expect(divide(6, 3)).toBe(2);
+      expect(divide(10, 0)).toBeNull();
+    });
+  });
 });
